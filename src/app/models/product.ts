4d56a796774c32ee154b5b0d8baef088b9471c18
Radhwen <radhwen.rmili@esprit.tn>
2023-04-15 14:16:44 +0100

    first commit

---
+++ src/app/models/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+    reference: string;
+    productName: string;
+    description: string;
+    quantity: number;
+    images: File;
+    model: File;
+    video: string;
+    brand: string;
+    price: number;
+    dateOfPurchase: Date;
+    discount: number;
+    yearsOfWarranty: number;
+    createdAt: Date;
+    productCategory: string;
+}
---
+++ src/app/services/product/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Product} from '../../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  readonly PRODUCT_API_URL = 'http://localhost:9092/COCO/api/product/';
+  constructor(private httpClient: HttpClient) { }
+  addProduct(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(this.PRODUCT_API_URL, product);
+  }
+}
